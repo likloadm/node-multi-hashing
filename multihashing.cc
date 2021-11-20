fdbd8d4ae721baa70656f1b73ff148bc34c6a732
Василий <bobit300@gmail.com>
2021-11-20 14:39:22 +0300

    add tidecoin

--- multihashing.cc
+++ multihashing.cc
@@ -157,7 +157,6 @@ using namespace v8;
  DECLARE_NO_INPUT_LENGTH_CALLBACK(yespower_0_5_R32, yespower_0_5_R32_hash, 32);
  DECLARE_NO_INPUT_LENGTH_CALLBACK(yespower_ltncg, yespower_ltncg_hash, 32);
  DECLARE_NO_INPUT_LENGTH_CALLBACK(yespower_r16, yespower_r16_hash, 32);
- DECLARE_NO_INPUT_LENGTH_CALLBACK(yespower_tide, yespower_tide_hash, 32);
 
 DECLARE_FUNC(scrypt) {
    DECLARE_SCOPE;
